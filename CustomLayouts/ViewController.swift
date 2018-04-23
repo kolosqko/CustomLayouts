97e1f623eb79f215a1491bd9e989f875af9164ea
Kolos <kolos@andrews-macbook-pro-2.local>
2018-04-23 19:06:29 +0300

    Initial Commit

---
+++ CustomLayouts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomLayouts
+//
+//  Created by Kolos on 4/23/18.
+//  Copyright © 2018 Kolos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
