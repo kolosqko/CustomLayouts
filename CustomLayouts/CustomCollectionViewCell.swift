19284209f2d6d3b3cb9279d51c18baabff579c01
kolos <kolosqko@gmail.com>
2018-04-24 11:15:25 +0300

    the commit

---
+++ CustomLayouts/CustomCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomCollectionViewCell.swift
+//  CustomLayouts
+//
+//  Created by Kolos on 4/23/18.
+//  Copyright © 2018 Kolos. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ CustomLayouts/CustomCollectionViewLayout.swift
@@ -0,0 +1,9 @@
+//
+//  CustomCollectionViewLayout.swift
+//  CustomLayouts
+//
+//  Created by Kolos on 4/23/18.
+//  Copyright © 2018 Kolos. All rights reserved.
+//
+
+import Foundation
